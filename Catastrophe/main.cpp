261690c2ceb7067a4b3a4909c6b5e9e227782475
Kerndog73 <kerndog73@gmail.com>
2017-07-09 12:42:34 +0930

    Remove swift stuff

---
+++ Catastrophe/main.cpp
@@ -0,0 +1,13 @@
+//
+//  main.cpp
+//  Catastrophe
+//
+//  Created by Indi Kernick on 9/7/17.
+//  Copyright © 2017 Indi Kernick. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int, char **) {
+  
+}
