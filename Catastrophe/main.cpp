eb8002c2f8c39c849e21f6b0641afde55a46bd3f
Kerndog73 <kerndog73@gmail.com>
2017-10-06 17:24:34 +1030

    Port pentagon sprite to LUA

--- Catastrophe/main.cpp
+++ Catastrophe/main.cpp
@@ -28,7 +28,7 @@ int main(int, char **) {
   app->mainLoop(timeStep, maxSteps);
   #endif*/
   
-  SpriteTestApp test("test sprite.lua", "main");
+  SpriteTestApp test("pentagon sprite.lua", "main");
   test.mainLoop(timeStep, maxSteps);
   
   PROFILER_INFO(stdout);
