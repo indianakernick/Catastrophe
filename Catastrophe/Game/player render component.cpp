2b73cb3cda29d8e94885c0139e72364e17d1bc6c
Kerndog73 <kerndog73@gmail.com>
2017-10-09 13:07:31 +1030

    Implement pivot animation in PlayerRenderComponent
    The implementation is very messy but it works. The player animation is not as good as vector sprite but I'll work on it.

--- Catastrophe/Game/player render component.cpp
+++ Catastrophe/Game/player render component.cpp
@@ -8,10 +8,14 @@
 
 #include "player render component.hpp"
 
+#include <vector>
 #include "entity.hpp"
+#include <glm/glm.hpp>
 #include "vector file.hpp"
 #include "yaml helper.hpp"
 #include "vector render.hpp"
+#include <Simpleton/Math/vectors.hpp>
+#include <Simpleton/Math/interpolate.hpp>
 #include "player animation component.hpp"
 #include <Simpleton/Utils/safe down cast.hpp>
 
@@ -25,33 +29,403 @@ void PlayerRenderComponent::init(NVGcontext *, RenderResMan &resMan) {
   sprite = loadSprite(spriteName, resMan);
 }
 
+namespace {
+  struct PivotJoint;
+  
+  using PivotJointPtr = std::shared_ptr<PivotJoint>;
+
+  struct PivotJoint {
+    template <typename ...Args>
+    PivotJoint(
+      const size_t index,
+      Args &&... args
+    ) : index(index), children({args...}) {}
+    
+    size_t index;
+    std::vector<PivotJointPtr> children;
+  };
+  
+  template <typename ...Args>
+  std::shared_ptr<PivotJoint> makePivotJoint(Args &&... args) {
+    return std::make_shared<PivotJoint>(std::forward<Args>(args)...);
+  }
+  
+        const auto rightHand = makePivotJoint(10);
+      const auto rightElbow  = makePivotJoint(9, rightHand);
+      const auto head        = makePivotJoint(8);
+        const auto leftHand  = makePivotJoint(7);
+      const auto leftElbow   = makePivotJoint(6, leftHand);
+    const auto neck          = makePivotJoint(5, leftElbow, head, rightElbow);
+      const auto rightFoot   = makePivotJoint(4);
+    const auto rightKnee     = makePivotJoint(3, rightFoot);
+      const auto leftFoot    = makePivotJoint(2);
+    const auto leftKnee      = makePivotJoint(1, leftFoot);
+  const auto hip             = makePivotJoint(0, leftKnee, rightKnee, neck);
+  
+  std::vector<float> swapLeftAndRight(const std::vector<float> angles) {
+    return {
+      angles[0],    // hip        -> hip
+      angles[3],    // rightKnee  -> leftKnee
+      angles[4],    // rightFoot  -> leftFoot
+      angles[1],    // leftKnee   -> rightKnee
+      angles[2],    // leftFoot   -> rightFoot
+      angles[5],    // neck       -> neck
+      angles[9],    // rightElbow -> leftElbow
+      angles[10],   // rightHand  -> leftHand
+      angles[8],    // head       -> head
+      angles[6],    // leftElbow  -> rightElbow
+      angles[7]     // leftHand   -> rightHand
+    };
+  }
+  
+  const std::vector<float> commonLengths = {
+    0.0f, // hip
+    4.0f, // leftKnee
+    3.0f, // leftFoot
+    4.0f, // rightKnee
+    3.0f, // rightFoot
+    5.0f, // neck
+    2.0f, // leftElbow
+    2.0f, // leftHand
+    1.0f, // head
+    2.0f, // rightElbow
+    2.0f  // rightHand
+  };
+  
+  struct Pose {
+    glm::vec2 pos;
+    std::vector<float> angles;
+  };
+  
+  const Pose standLeftPose = {
+    {1.0f, -1.0f},
+    {
+      0,    // hip
+      165,  // leftKnee
+      15,   // leftFoot
+      180,  // rightKnee
+      20,   // rightFoot
+      0,    // neck
+      205,  // leftElbow
+      -25,  // leftHand
+      0,    // head
+      180,  // rightElbow
+      -25   // rightHand
+    }
+  };
+  const Pose standRightPose = {
+    {1.0f, -1.0f},
+    swapLeftAndRight(standLeftPose.angles)
+  };
+  
+  const Pose runJumpLeftPose = {
+    {0.0f, 0.0f},
+    {
+      0,    // hip
+      105,  // leftKnee
+      20,   // leftFoot
+      225,  // rightKnee
+      20,   // rightFoot
+      15,   // neck
+      230,  // leftElbow
+      -45,  // leftHand
+      -15,  // head
+      140,  // rightElbow
+      -110, // rightHand
+    }
+  };
+  const Pose runJumpRightPose = {
+    {0.0f, 0.0f},
+    swapLeftAndRight(runJumpLeftPose.angles)
+  };
+  
+  const Pose standJumpLeftPose = {
+    {0.0f, 0.0f},
+    {
+      0,    // hip
+      75,   // leftKnee
+      105,  // leftFoot
+      105,  // rightKnee
+      150,  // rightFoot
+      0,    // neck
+      225,  // leftElbow
+      -45,  // leftHand
+      0,    // head
+      135,  // rightElbow
+      -90   // rightHand
+    }
+  };
+  const Pose standJumpRightPose = {
+    {0.0f, 0.0f},
+    swapLeftAndRight(standJumpLeftPose.angles)
+  };
+  
+  struct Keyframe {
+    float offset;
+    Pose data;
+  };
+  
+  struct Animation {
+    float duration;
+    std::vector<Keyframe> keyframes;
+  };
+  
+  const Animation runAnimation = [] () -> Animation {
+    const Pose run0 = {
+      {0.0f, -2.0f},
+      {
+        0,    // hip
+        140,  // leftKnee
+        120,  // leftFoot
+        205,  // rightKnee
+        20,   // rightFoot
+        15,   // neck
+        200,  // leftElbow
+        -90,  // leftHand
+        -15,  // head
+        200,  // rightElbow
+        -90   // rightHand
+      }
+    };
+    const Pose run1 = {
+      {0.0f, -1.0f},
+      {
+        0,    // hip
+        120,  // leftKnee
+        20,   // leftFoot
+        210,  // rightKnee
+        60,   // rightFoot
+        15,   // neck
+        230,  // leftElbow
+        -90,  // leftHand
+        -15,  // head
+        165,  // rightElbow
+        -90   // rightHand
+      }
+    };
+    const Pose run2 = {
+      {0.0f, -2.0f},
+      {
+        0,    // hip
+        135,  // leftKnee
+        20,   // leftFoot
+        225,  // rightKnee
+        50,   // rightFoot
+        15,   // neck
+        210,  // leftElbow
+        -90,  // leftHand
+        -15,  // head
+        160,  // rightElbow
+        -90   // rightHand
+      }
+    };
+    const Pose run3 = {
+      {0.0f, -1.0f},
+      {
+        0,    // hip
+        160,  // leftKnee
+        40,   // leftFoot
+        200,  // rightKnee
+        90,   // rightFoot
+        15,   // neck
+        225,  // leftElbow
+        -90,  // leftHand
+        -15,  // head
+        165,  // rightElbow
+        -90   // rightHand
+      }
+    };
+    
+    return {1.0f, {
+      {0.0f, run0},
+      {0.125f, run1},
+      {0.25f, run2},
+      {0.375f, run3},
+      {0.5f, {run0.pos, swapLeftAndRight(run0.angles)}},
+      {0.625f, {run1.pos, swapLeftAndRight(run1.angles)}},
+      {0.75f, {run2.pos, swapLeftAndRight(run2.angles)}},
+      {0.875f, {run3.pos, swapLeftAndRight(run3.angles)}},
+      {1.0f, run0}
+    }};
+  }();
+  
+  void pivotPoints(
+    std::vector<glm::vec2> &points,
+    const std::vector<float> &angles,
+    const std::vector<float> &lengths,
+    const PivotJoint &node,
+    const glm::vec2 pos,
+    float angle
+  ) {
+    if (angles.size() != lengths.size()) {
+      throw std::runtime_error("Angles and lengths must be of equal size");
+    }
+    if (points.size() != angles.size()) {
+      points.resize(angles.size());
+    }
+    if (node.index >= points.size()) {
+      throw std::runtime_error("Node index out of range");
+    }
+  
+    angle += angles[node.index];
+    const glm::vec2 newPos = pos + Math::angleMag(
+      glm::radians(-angle),
+      lengths[node.index]
+    );
+    points[node.index] = newPos;
+    for (auto &c : node.children) {
+      pivotPoints(points, angles, lengths, *c, newPos, angle);
+    }
+  }
+  
+  void lerpPose(
+    Pose &dst,
+    const float t,
+    const Pose &min,
+    const Pose &max
+  ) {
+    if (min.angles.size() != max.angles.size()) {
+      throw std::runtime_error("Cannot interpolate poses of different sizes");
+    }
+    if (dst.angles.size() != min.angles.size()) {
+      dst.angles.resize(min.angles.size());
+    }
+    dst.pos = Math::lerp(t, min.pos, max.pos);
+    for (size_t a = 0; a != dst.angles.size(); ++a) {
+      dst.angles[a] = Math::lerp(t, min.angles[a], max.angles[a]);
+    }
+  }
+  
+  void lerpKeyframe(
+    Pose &dst,
+    const float t,
+    const Keyframe &min,
+    const Keyframe &max
+  ) {
+    return lerpPose(
+      dst,
+      Math::invLerp<float>(t, min.offset, max.offset),
+      min.data,
+      max.data
+    );
+  }
+  
+  void lerpAnimation(Pose &dst, const float t, const Animation &anim) {
+    const size_t size = anim.keyframes.size();
+    
+    if (size == 0) {
+      throw std::runtime_error("Cannot interpolate an animation with no keyframes");
+    } else if (size == 1) {
+      dst = anim.keyframes[0].data;
+      return;
+    } else if (size == 2) {
+      lerpKeyframe(dst, t, anim.keyframes[0], anim.keyframes[1]);
+      return;
+    }
+    
+    if (t < anim.keyframes[0].offset) {
+      lerpKeyframe(dst, t, anim.keyframes[0], anim.keyframes[1]);
+      return;
+    } else if (t > anim.keyframes[size - 1].offset) {
+      lerpKeyframe(dst, t, anim.keyframes[size - 2], anim.keyframes[size - 1]);
+      return;
+    }
+    
+    //Probably don't need to bother with std::lower_bound
+    for (size_t k = 1; k != size; ++k) {
+      if (anim.keyframes[k].offset >= t) {
+        lerpKeyframe(dst, t, anim.keyframes[k - 1], anim.keyframes[k]);
+        return;
+      }
+    }
+    throw std::runtime_error("Unsorted keyframe offsets");
+  }
+  
+  void nvgMoveTo(NVGcontext *const ctx, const glm::vec2 point) {
+    nvgMoveTo(ctx, point.x, point.y);
+  }
+  void nvgLineTo(NVGcontext *const ctx, const glm::vec2 point) {
+    nvgLineTo(ctx, point.x, point.y);
+  }
+  void nvgCircle(NVGcontext *const ctx, const glm::vec2 point, const float radius) {
+    nvgCircle(ctx, point.x, point.y, radius);
+  }
+}
+
 void PlayerRenderComponent::render(NVGcontext *const ctx) {
   const auto animComp = Utils::safeDownCast<PlayerAnimationComponent>(
     getEntity().animation
   );
   
-  const float leg = animComp->getRightLeg() ? 1.0f : 0.0f;
-  
-  Frame frame = ::getFrame(sprite, "stand", leg);
-  lerpFrames(
+  static Pose standGroundJump;
+  lerpPose(
+    standGroundJump,
     animComp->getGroundJumpProg(),
-    frame,
-    ::getFrame(sprite, "stand jump", leg)
+    animComp->getRightLeg() ? standRightPose : standLeftPose,
+    animComp->getRightLeg() ? standJumpRightPose : standJumpLeftPose
   );
   
-  Frame runningFrame = ::getFrame(sprite, "run", animComp->getRunningProg());
-  lerpFrames(
+  static Pose running;
+  lerpAnimation(running, animComp->getRunningProg(), runAnimation);
+  
+  static Pose runningGroundJump;
+  lerpPose(
+    runningGroundJump,
     animComp->getGroundJumpProg(),
-    runningFrame,
-    ::getFrame(sprite, "run jump", leg)
+    running,
+    animComp->getRightLeg() ? runJumpRightPose : runJumpLeftPose
   );
   
-  lerpFrames(
+  static Pose pose;
+  lerpPose(
+    pose,
     animComp->getStandRunProg(),
-    frame,
-    runningFrame
+    standGroundJump,
+    runningGroundJump
   );
+  
+  static std::vector<glm::vec2> points;
+  pivotPoints(points, pose.angles, commonLengths, *hip, pose.pos, -90);
   
   setModelTransform(ctx);
-  renderSprite(ctx, sprite, frame);
+  
+  nvgScale(ctx, 0.0625f, 0.0625f);
+  nvgLineCap(ctx, NVG_ROUND);
+  nvgLineJoin(ctx, NVG_ROUND);
+  nvgStrokeWidth(ctx, 1.0f);
+  
+  nvgBeginPath(ctx);
+  nvgStrokeColor(ctx, nvgRGBf(0.75f, 0.75f, 0.75f));
+  nvgMoveTo(ctx, points[neck->index]);
+  nvgLineTo(ctx, points[leftElbow->index]);
+  nvgLineTo(ctx, points[leftHand->index]);
+  nvgMoveTo(ctx, points[hip->index]);
+  nvgLineTo(ctx, points[leftKnee->index]);
+  nvgLineTo(ctx, points[leftFoot->index]);
+  nvgStroke(ctx);
+  
+  nvgBeginPath(ctx);
+  nvgStrokeColor(ctx, nvgRGBf(0.5f, 0.5f, 0.5f));
+  nvgMoveTo(ctx, points[hip->index]);
+  nvgLineTo(ctx, points[rightKnee->index]);
+  nvgLineTo(ctx, points[rightFoot->index]);
+  nvgStroke(ctx);
+  
+  nvgBeginPath(ctx);
+  nvgStrokeColor(ctx, nvgRGBf(1.0f, 1.0f, 1.0f));
+  nvgMoveTo(ctx, points[hip->index]);
+  nvgLineTo(ctx, points[neck->index]);
+  nvgStroke(ctx);
+  
+  nvgBeginPath(ctx);
+  nvgStrokeColor(ctx, nvgRGBf(0.5f, 0.5f, 0.5f));
+  nvgMoveTo(ctx, points[neck->index]);
+  nvgLineTo(ctx, points[rightElbow->index]);
+  nvgLineTo(ctx, points[rightHand->index]);
+  nvgStroke(ctx);
+  
+  nvgBeginPath(ctx);
+  nvgFillColor(ctx, nvgRGBf(1.0f, 1.0f, 1.0f));
+  nvgCircle(ctx, points[head->index], 1.5f);
+  nvgFill(ctx);
 }
